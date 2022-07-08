534e698a0a155d772ffe904f6b01ff9d9f78ff1d
Anelize <anelizepelloso00@gmail.com>
2022-07-08 10:00:07 -0300

    Java em desenvolvimento

--- java.js
+++ java.js
@@ -2,8 +2,9 @@
 //let username = prompt("Qual o seu nome?");
 let userLink;
 let itemSelected;
+let conteudo = [];
 //FIM DA VARIÁVEIS GLOBAIS
-
+setInterval(inputValue, 1000);
 //Função que seleciona
 function selecection(itemSelected){ 
     let variavel = itemSelected.querySelector(".backgroundGrey");   
@@ -15,24 +16,28 @@ function selecection(itemSelected){
         }
     }
     variavel.classList.add("borderBlue");
+    //verification();
 }
 //Fim da função que seleciona
 
 function verification(){
     //Essa funçao checa se os itens estão selecionados e se a funçao input não esta vazia
-    // se as condiçoes forem verdadeiras, ele deixa o botao de confirmação azul;    
+    // se as condiçoes forem verdadeiras, ele deixa o botao de confirmação azul; 
+    let modelo = document.querySelector(".model .borderBlue");   
+    let gola = document.querySelector(".collar .borderBlue");  
+    let tecido = document.querySelector(".tissue .borderBlue");
+    let confirmacao = document.querySelector(".confirm");    
+    let condicao2 = (userLink == '');
+    let condicao1 = (modelo != null)&&(gola != null)&&(tecido != null);
+    if(condicao1 && !condicao2){
+        confirmacao.classList.add("buttonBlue");
+    }       
 }
-
-//Armazenando link do usuario
-function link(){
-    //usar um if e verificar se não está vazia
+function inputValue(){
     let input = document.querySelector("input");
-    userLink = input.value;
-    if(userLink == null){
-        alert("Você deve inserir um link!");
-        link();
-    }
+    userLink = input.value;  
+    verification()  
 }
-//Fim
-
-
+function sendRequest(){
+    alert("Sua encomenda foi realizada com sucesso!");
+}
